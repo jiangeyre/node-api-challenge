d9d5253e67191c15eff0a534dc72006a4c3d4723
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-17 09:39:53 -0800

    Commit 8: made a cra but I am having a CORS issue

--- client/node-challenge/src/App.js
+++ client/node-challenge/src/App.js
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// styling 
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+  const [actions, setActions] = useState();
+  const [projects, setProjects] = useState();
+
+  useEffect(() => {
+    axios.get("http://localhost:8000")
+      .then(res => {
+        setData(res.data);
+        console.log(res);
+        console.log(data);
+      })
+  }, [data, setData]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/actions')
+      .then(res => {
+        setActions(res.data);
+      })
+  }, [setActions]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/projects')
+      .then(res => {
+        setProjects(res.data);
+      })
+  }, [setProjects]);
+
+  const deleteActButt = (id) => {
+    axios.delete(`http://localhost:8000/api/actions/${id}`)
+      .then(res => {
+        console.log(res);
+        setActions(actions.filter((item, ind) =>
+        item.id !== id))
+      })
+      .catch(err => console.log(err))
+  };
+
+  const deleteProjButt = (id) => {
+    axios.delete(`http://localhost:8000/api/projects/${id}`)
+      .then(res => {
+        console.log(res);
+        setProjects(projects.filter((item, ind) =>
+        item.id !== id))
+      })
+      .catch(err => console.log(err))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data ? <h1>{data.message}</h1> : <h1></h1>}
+      {actions ? (
+        <div>
+          {actions.map((act, ind) => (
+            <div>
+              <h1>{act.id}</h1>
+              <h2>{act.project_id}</h2>
+              <h2>{act.description}</h2>
+              <h2>{act.notes}</h2>
+              <h2>{act.completed}</h2>
+              <button onClick={() => {deleteActButt(act.id)}}>Delete</button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h1></h1>
+      )}
+      {projects ? (
+        <div>
+          {projects.map((proj, ind) => (
+            <div>
+              <h1>{proj.id}</h1>
+              <h2>{proj.project_id}</h2>
+              <h2>{proj.description}</h2>
+              <h2>{proj.notes}</h2>
+              <h2>{proj.completed}</h2>
+              <button onClick={() => {deleteProjButt(proj.id)}}>Delete</button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h1></h1>
+      )}
     </div>
   );
 }
